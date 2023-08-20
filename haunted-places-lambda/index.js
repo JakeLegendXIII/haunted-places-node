db457289c8551f80f2c22cadaedfb7cdf96e4d43
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-08-20 18:15:23 -0400

    Lambda to node 18. packages and switch to esm

--- haunted-places-lambda/index.js
+++ haunted-places-lambda/index.js
@@ -1,42 +1,39 @@
-let { Client } = require('redis-om');
-const AWS = require('aws-sdk');
+import { Client } from 'redis-om';
 
-exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
     try {
         let client = new Client();
         await client.open(getEnvironmentVariable('REDIS_CONNECTION'));
         let searchText = BuildSearchText(event);
         let index = getEnvironmentVariable('REDIS_SEARCH_INDEX_NAME');
-        // LIMIT 0 0 returns just the count
-        let countResult = await client.execute(['FT.SEARCH' , index, searchText, "LIMIT", 0, 0]);
+         // LIMIT 0 0 returns just the count
+        let countResult = await client.execute(['FT.SEARCH', index, searchText, "LIMIT", 0, 0]);
         // LIMIT 0 N returns all the rows between the offset of 0 and the number N - offset is zero-indexed
-        let queryResult = await client.execute(['FT.SEARCH' , index, searchText, "LIMIT", 0, countResult[0]]);
+        let queryResult = await client.execute(['FT.SEARCH', index, searchText, "LIMIT", 0, countResult[0]]);
         await client.close();
 
         let response = buildResponse(queryResult);
-        return { statusCode: 200,
+        return {
+            statusCode: 200,
             body: JSON.stringify(response)
         };
 
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err);
         return {
             statusCode: 500,
-            body: {
+            body: JSON.stringify({
                 errorMessage: err.message
-            }
+            })
         }
     }
-}
+};
 
 const buildResponse = (queryResult) => {
-    // The execute gives us a pretty nested set of hashes we can iterate through
     let itemCount = queryResult[0];
     let body = [];
     Object.keys(queryResult).forEach(function (key) {
-        if (key > 0)
-        {
+        if (key > 0) {
             var value = queryResult[key];
             Object.keys(value).forEach(function (key2) {
                 var innerValue = value[key2];
@@ -57,8 +54,8 @@ const buildResponse = (queryResult) => {
 
 const BuildSearchText = (event) => {
     // '@location:[-84.5120 39.1031 50 mi]' example
-    // Redis is backwards longitude first then latitude    
-    return `@location:[${event.queryStringParameters.longitude} ${event.queryStringParameters.latitude} ${event.queryStringParameters.radiusInMiles} mi]`
+    // Redis is backwards longitude first then latitude 
+    return `@location:[${event.queryStringParameters.longitude} ${event.queryStringParameters.latitude} ${event.queryStringParameters.radiusInMiles} mi]`;
 }
 
 const getEnvironmentVariable = (variableName) => {
--- haunted-places-lambda/test-runner.js
+++ haunted-places-lambda/test-runner.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
+import 'dotenv/config';
 
-const entry = require('./index');
+import { handler } from './index.js';
 
-// This lambda is triggered via an API Gateway proxy and passes an event
-entry.handler(
-    {
-        resource: '/hauntedplaces',
-        path: '/hauntedplaces',
-        httpMethod: 'GET',
-        queryStringParameters: { radiusInMiles: '20', latitude: '39.1031', longitude: '-84.5120' },
-        multiValueQueryStringParameters: {
-          radiusInMiles: [ '20' ],
-          latitude: [ '39.1031' ],
-          longitude: [ '-84.5120' ]
-        },
-        pathParameters: null,      
-        body: null,
-        isBase64Encoded: false
-    }
-, {}).then(response => console.log(`Lambda response: ${JSON.stringify(response)}`));
+const event = {
+    resource: '/hauntedplaces',
+    path: '/hauntedplaces',
+    httpMethod: 'GET',
+    queryStringParameters: { radiusInMiles: '20', latitude: '39.1031', longitude: '-84.5120' },
+    multiValueQueryStringParameters: {
+      radiusInMiles: [ '20' ],
+      latitude: [ '39.1031' ],
+      longitude: [ '-84.5120' ]
+    },
+    pathParameters: null,      
+    body: null,
+    isBase64Encoded: false
+};
+
+handler(event, {}).then(response => console.log(`Lambda response: ${JSON.stringify(response)}`));
